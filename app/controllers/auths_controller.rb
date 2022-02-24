b84998ffae602356f2f15fbf6326938adaa267f2
EdmilsonOSJr <edmilsonosjr@gmail.com>
2022-02-24 12:34:03 -0300

    add tempo de vida para o token

--- app/controllers/auths_controller.rb
+++ app/controllers/auths_controller.rb
@@ -4,7 +4,7 @@ class AuthsController < ApplicationController
     def create 
 
         # The secret must be a string. A JWT::DecodeError will be raised if it isn't provided.
-        payload = { name: params[:name]}
+        payload = { name: params[:name], exp: Time.now.to_i + 300 }
         token = JWT.encode payload, Auth::HMAC_SECRET, 'HS256'
 
         render json: { token: token }
